33625868a22ed2e6bcb03502b81ad899412dcb0d
Akul Santhosh <akul753@gmail.com>
2020-03-11 00:25:34 +0530

    All files

---
+++ js/data.js
@@ -0,0 +1,147 @@
+let data = {
+    "Home": {
+        "title": "Hi I am Your Name",
+        "image": "<img src='https://share.getcloudapp.com/items/d5u0RkDy/download'>",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac.",
+        "address": "Line 1 address<br>Line 2 address<br>Line 3 address<br>Line 4 address",
+        "social": {
+            "Email : ": "<a href='mailto:emailaddress?Subject=Hello' target='_top'>Address of email</a>",
+            "Phone No : ": "<a href='tel:000000000'>no</a>",
+            "LinkedIn : ": "<a href='#'>link</a>",
+            "Twitter : ": "<a href='#'>link</a>",
+            // "Quora : ": "<a href='https://www.quora.com/profile/Akul-Santhosh'>Q&A AkulS</a>"
+        }
+    },
+    "CV": {
+        "title": "To download offline version click <a href='#'>here</a>",
+        "list": {
+            "Education": {
+                "institution": {
+                    "title": "Name of college",
+                    "subtitle": "Bachelor of Technology in Computer Science",
+                    "date": "August 2016 - May 2020",
+                    "description": "<b>CGPA - 9.07 /10, Ranked 7th in class.</b> <br>Core Courses:- Operating System, Algorithms, Data Structures, Object Oriented Programming Language, Computer Architecture and Organisation, Principles of Programming Languages, Data Mining, Graphics, Automata Languages."
+                }
+            },
+            "Startup": {
+                "trebuchet": {
+                    "title": "Name",
+                    "subtitle": "Cofounder",
+                    "date": "September 2018 - Present",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                }
+            },
+            "Internship": {
+                "wipro": {
+                    "title": "Name",
+                    "subtitle": "Research Intern ",
+                    "date": "June 2019 - July 2019",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "hal": {
+                    "title": "Name",
+                    "subtitle": "Intern for Information Technology Division",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                }
+            },
+            "Projects": {
+                "listitem1": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitem2": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitel3": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac. Nullam euismod facilisis tellus, ac finibus nisi ornare pellentesque. Morbi non lectus sed nisi porta venenatis at nec quam. Mauris diam nibh, feugiat nec orci vel, efficitur facilisis magna. Duis risus mi, pharetra vitae ornare sed, faucibus a sapien."
+                }
+            },
+            "Leadership/Volunteering": {
+                "listitem1": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitem2": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitel3": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac. Nullam euismod facilisis tellus, ac finibus nisi ornare pellentesque. Morbi non lectus sed nisi porta venenatis at nec quam. Mauris diam nibh, feugiat nec orci vel, efficitur facilisis magna. Duis risus mi, pharetra vitae ornare sed, faucibus a sapien."
+                }
+            },
+            "Achievments/Certifications": {
+                "listitem1": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitem2": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac."
+                },
+                "listitel3": {
+                    "title": "Name",
+                    "subtitle": "subtitle",
+                    "date": "May 2018 - June 2018",
+                    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu ipsum quis risus pellentesque vehicula efficitur quis velit. Maecenas interdum lorem diam. Quisque laoreet dictum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam a ante vel sapien blandit consequat. Suspendisse non eleifend dolor. Donec aliquet, mauris eget porta semper, tortor felis pharetra orci, nec iaculis metus enim at felis. Suspendisse eleifend eleifend quam, quis viverra ipsum fringilla ac. Nullam euismod facilisis tellus, ac finibus nisi ornare pellentesque. Morbi non lectus sed nisi porta venenatis at nec quam. Mauris diam nibh, feugiat nec orci vel, efficitur facilisis magna. Duis risus mi, pharetra vitae ornare sed, faucibus a sapien."
+                }
+            }
+        }
+    },
+    "Moments": {
+        "image1": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/d5u0RkDy/download"
+        },
+        "image2": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/DOu8Byzn/download"
+        },
+        "image3": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/d5u0RkDy/download"
+        },
+        "image4": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/DOu8Byzn/download"
+        },
+        "image5": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/d5u0RkDy/download"
+        },
+        "image6": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/DOu8Byzn/download"
+        },
+        "image7": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/d5u0RkDy/download"
+        },
+        "image8": {
+            "title": "your caption",
+            "url": "https://share.getcloudapp.com/items/DOu8Byzn/download"
+        }
+    }
+    // "Blog": {
+    //     "title": "Check back soon"
+    // }
+}
